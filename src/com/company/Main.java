8f95e6fac566458efabf67ead967b6e9a5b15748
Edvin Gavell <eddi@live.se>
2020-09-10 16:02:47 +0200

    Simplified code and did an effort to more nice looking.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
-            if (i % 3 ==0 && i % 5 ==0) {
-                System.out.println("FizzBuzz");
-            }
-            if (i == 42){
+            if (i == 42) {
                 System.out.println("Answer to the question of life, the universe, and everything");
-                } else if (i % 5 ==0) {
+            } else if (i % 3 ==0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 ==0) {
                 System.out.println("Buzz");
-                } else {
+            } else {
                 System.out.println(i);
             }
-
-
         }
     }
 }
