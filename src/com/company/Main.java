6d6c509749614801b3abfad303ad7d8cf745aebd
Andreas Berg <andreas.berg2@student.kyh.se>
2020-09-10 15:46:32 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            if (i % 3 ==0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            }
+            if (i == 42){
+                System.out.println("Answer to the question of life, the universe, and everything");
+                } else if (i % 5 ==0) {
+                System.out.println("Buzz");
+                } else {
+                System.out.println(i);
+            }
+
+
+        }
+    }
+}
